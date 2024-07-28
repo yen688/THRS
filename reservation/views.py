23599a99234319adace3b2c78ebf1a73aabd96f6
a8861501 <a8861501@gmail.com>
2024-07-28 16:09:58 +0800

    Debug for rule page

--- reservation/views.py
+++ reservation/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'reservation/index.html')
+
+def rule(request):
+    return render(request, 'reservation/rule.html')
