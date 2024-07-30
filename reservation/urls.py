a0ac238f7390649220a726df2f3963df9d8b2b00
a8861501 <a8861501@gmail.com>
2024-07-30 23:26:09 +0800

    Add confirm page Get confirm form data

--- reservation/urls.py
+++ reservation/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('rule', views.rule, name='rule'),
+    path('confirmForm', views.confirmForm, name='confirmReservation'),
+    path('confirm', views.confirm, name='confirm'),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -6,3 +6,12 @@ def index(request):
 
 def rule(request):
     return render(request, 'reservation/rule.html')
+
+def confirmForm(request):
+    return render(request, 'reservation/confirmForm.html')
+
+def confirm(request):
+    if request.method == 'POST':
+        customer = request.POST.get('customer')
+        phone = request.POST.get('phone')
+    return render(request, 'reservation/index.html')
